81df2b4fa7f8c2e0b2de4ccbcef558e0ce414ce3
xfye <chnos@163.com>
2016-11-26 11:21:02 +0800

    add compiling .py in build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,23 @@ cp -r src/node ${workdir}
 cp -r src/lib ${workdir}
 
 cd $workdir
+
+# compile lib
 git clone git@bitbucket.org:idevops/python-etcd.git
 cd python-etcd && git checkout 0.4.3
 cp -r src/etcd $workdir/lib
+python -mcompileall $workdir/lib/ido
+rm $workdir/lib/ido/*.py
 cp -r $workdir/lib $workdir/master
 cp -r $workdir/lib $workdir/node
+
+# compile master
+python -mcompileall $workdir/master/bin/idoctl.py
+rm $workdir/master/bin/idoctl.py
+
+# compile node
+python -mcompileall $workdir/node/bin/nodectl.py
+rm $workdir/node/bin/nodectl.py
 
 cd $workdir
 tar czvf ido-master.tar.gz master
